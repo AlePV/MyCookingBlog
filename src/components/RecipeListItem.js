cf90c8a5a364fc1e7e8b8edc3658ce1797cfda13
Mariale Pestana <marialepestana@Mariales-MacBook-Pro.local>
2018-08-04 17:28:18 -0500

    Created RecipeListItem file

---
+++ src/components/RecipeListItem.js
@@ -0,0 +1,16 @@
+import React from "react";
+import {Link} from "react-router-dom";
+import moment from "moment";
+
+const RecipeListitem = ({id, recipeName, ingredients, procedure, createdAt}) => {
+
+    <Link to={`/edit/${id}`}>
+        <div>
+            <h3>{recipeName}</h3>
+
+            <span>{moment(createdAt).format("M / D / YYYY")}</span>
+        </div>
+    </Link>
+};
+
+export default RecipeListitem;
